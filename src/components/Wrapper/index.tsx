83e6665a8b256b9a525b990ac26537412e0d2d9f
Diana Tuz <dianatuz12@gmail.com>
2024-12-27 17:59:03 +0200

    Add base components

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,13 +1,24 @@
 import { Outlet } from 'react-router-dom'
 import styled from 'styled-components'
+import { Header } from '../Header'
 
 export const Wrapper = () => (
   <Container>
-    <header>{'header'}</header>
-    <Outlet />
+    <Header />
+    <Main>
+      <Outlet />
+    </Main>
     <footer>{'footer'}</footer>
   </Container>
 )
 
-const Container = styled.div``
-// const H1 = styled.h1``;
+const Container = styled.div`
+  background-color: var(--primary);
+  color: var(--white);
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.main`
+  flex: 1;
+`
